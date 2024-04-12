6ca9d6f47dc324c1811698c7e5926447d2fdbbc1
Rijk Mulder <mulderrijk@gmail.com>
2024-04-12 10:02:36 +0200

    Fish fix

--- Assets/Scripts/FishingRot/Dubber.cs
+++ Assets/Scripts/FishingRot/Dubber.cs
@@ -12,8 +12,10 @@ public class Kurwa : MonoBehaviour
     [SerializeField] private GameObject Originpoint;
     [SerializeField] private float MoveSpeed;
     [SerializeField] private Material material;
+    [SerializeField] private Vector3 offset;
     private FisherMan Fisher;
     private float timer;
+    float speed;
     private void Start()
     {
         Fisher = FindObjectOfType<FisherMan>();
@@ -22,16 +24,25 @@ public class Kurwa : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Mouse1) || Input.GetKey(KeyCode.UpArrow))
         {
-            if (bobber.state == BobberState.Caught)timer += Time.deltaTime * MoveSpeed;
-            Hook.useGravity = false;
+            if (bobber.state == BobberState.Caught)
+            {
+                timer += Time.deltaTime * MoveSpeed;
+                speed = MoveSpeed - timer;
+            }
+            else
+            {
+                speed = MoveSpeed;
+                timer = 0;
+            }
 
-            float speed = MoveSpeed - timer;
-            if (speed <= 0)
+            if (speed <= 0 && speed >= -1)
             {
                 material = GetMaterial(true);
                 TrackBobberBehaviour fish = bobber.GetComponentInChildren<TrackBobberBehaviour>();
                 fish.InvokeFishCaught();
             }
+            Hook.useGravity = false;
+            
             transform.position = Vector3.MoveTowards(transform.position, Originpoint.transform.position, speed * Time.deltaTime);
             Hook.transform.position = transform.position;
             if (Vector3.Distance(transform.position, Originpoint.transform.position) < Fisher.DistanceToRot)
@@ -46,7 +57,7 @@ public class Kurwa : MonoBehaviour
             timer -= Time.deltaTime * MoveSpeed * 3;
             timer = Mathf.Clamp(timer, 0, 10);
             material = GetMaterial(false);
-            transform.position = Hook.transform.position;
+            transform.position = Hook.transform.position + offset;
         }
     }
     private Material GetMaterial(bool state)
@@ -55,9 +66,9 @@ public class Kurwa : MonoBehaviour
 
         if (state)
         {
-            mat.color = Color.red;
+            mat.color = new Color(255, 0, 0, 0.6f);
         }
-        else mat.color = Color.black;
+        else mat.color = new Color(0, 0, 0, 0.6f);
         return mat;
     }
 }
