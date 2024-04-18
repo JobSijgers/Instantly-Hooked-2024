75630bd7d2e729befb1a3853dd59e4e2c636bc89
Job SIjgers <jobsijgers@gmail.com>
2024-04-18 09:24:01 +0200

    Boat Works

--- Assets/Scripts/Boat/BoatController.cs
+++ Assets/Scripts/Boat/BoatController.cs
@@ -1,12 +1,46 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class BoatController : MonoBehaviour
+namespace Boat
 {
-    private void Update()
+    [RequireComponent(typeof(Rigidbody))]
+    public class BoatController : MonoBehaviour
     {
-        
+        [SerializeField] private float maxVelocity;
+        [SerializeField] private float speedMultiplier;
+        private Rigidbody _rigidbody;
+        private float _input;
+
+        private void Start()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+        }
+
+        private void Update()
+        {
+            _input = GetBoatInput();
+        }
+
+        private void FixedUpdate()
+        {
+            MoveBoat(_input);
+        }
+
+        private static float GetBoatInput()
+        {
+            return Input.GetAxisRaw("Horizontal");
+        }
+
+        private void MoveBoat(float input)
+        {
+            if (input == 0) return;
+            _rigidbody.AddForce(Vector3.right * (input * speedMultiplier));
+
+            //check if velocity succeeds max speed and counteracts it.
+            if (_rigidbody.velocity.magnitude < maxVelocity) return;
+
+            var velocity = _rigidbody.velocity;
+            var counteractForce = velocity.normalized * (maxVelocity - velocity.magnitude);
+            _rigidbody.AddForce(counteractForce);
+        }
     }
 }
