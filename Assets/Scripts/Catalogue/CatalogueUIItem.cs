f3118514629e2c333f7c07b58ff369f3b2d53bcc
Job Sijgers <jobsijgers@gmail.com>
2024-05-10 10:46:41 +0200

    Catalogue mostly works

---
+++ Assets/Scripts/Catalogue/CatalogueUIItem.cs
@@ -0,0 +1,27 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Catalogue
+{
+    public class CatalogueUIItem : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text fishNameText;
+        [SerializeField] private TMP_Text fishDescriptionText;
+        [SerializeField] private TMP_Text fishAmountText;
+        [SerializeField] private TMP_Text fishHabitatText;
+        [SerializeField] private Image fishRarityImage;
+        [SerializeField] private Image fishImage;
+
+        public void Initialize(string fishName, string fishDescription, int fishAmount, string fishHabitat,
+            Sprite fishRaritySprite, Sprite fishSprite)
+        {
+            fishNameText.text = fishName;
+            fishDescriptionText.text = fishDescription;
+            fishAmountText.text = $"{fishAmount} Caught";
+            fishHabitatText.text = fishHabitat;
+            fishRarityImage.sprite = fishRaritySprite;
+            fishImage.sprite = fishSprite;
+        }
+    }
+}
