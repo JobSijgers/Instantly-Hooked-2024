6013b0782d346ed3a3d480af8e678e4b79f99f98
Job Sijgers <jobsijgers@gmail.com>
2024-05-13 23:06:24 +0200

    game somewhat ready for playtest

---
+++ Assets/Scripts/Tutorial/NoInputVisual.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Events;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+namespace Tutorial
+{
+    public class NoInputVisual : MonoBehaviour
+    {
+        [SerializeField] private Image visual;
+        [SerializeField] private float display;
+        private float t;
+        private float depth;
+
+        private void Start()
+        {
+            EventManager.DepthUpdate += OnDepthUpdate;
+        }
+
+        private void OnDestroy()
+        {
+            EventManager.DepthUpdate -= OnDepthUpdate;
+        }
+
+
+        private void Update()
+        {
+            if (depth <= 0 )
+            {
+                visual.enabled = false;
+                return;
+            }
+            
+            if (!Input.anyKey)
+            {
+                float a = t + Time.deltaTime;
+                t = Mathf.Clamp(a, 0, display);
+            }
+            else
+            {
+                t = 0;
+            }
+
+            visual.enabled = t >= display;
+        }
+        
+        private void OnDepthUpdate(float depth)
+        {
+            this.depth = depth;
+        }
+        
+    }
+}
