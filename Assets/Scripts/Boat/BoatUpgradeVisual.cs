ee24e3d76f21a4bd1dd7ea2ab96ed1ac6c4388f0
Job SIjgers <jobsijgers@gmail.com>
2024-05-21 12:03:27 +0200

    Added upgrade visuals

---
+++ Assets/Scripts/Boat/BoatUpgradeVisual.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Events;
+using UnityEngine;
+using Upgrades;
+
+namespace Boat
+{
+    public class BoatUpgradeVisual : MonoBehaviour
+    {
+        [Serializable]
+        private class UpgradeVisual
+        {
+            public Upgrade upgrade;
+            public MeshFilter meshFilter;
+            public MeshRenderer meshRenderer;
+        }
+        
+        [SerializeField] private UpgradeVisual[] upgradeVisuals;
+
+        private void Start()
+        {
+            EventManager.UpgradeBought += UpdateVisuals;
+        }
+
+        private void OnDestroy()
+        {
+            EventManager.UpgradeBought -= UpdateVisuals;
+        }
+        
+        private void UpdateVisuals(Upgrade upgrade)
+        {
+            foreach (UpgradeVisual upgradeVisual in upgradeVisuals)
+            {
+                if (upgradeVisual.upgrade != upgrade) continue;
+                
+                upgradeVisual.meshFilter.mesh = upgrade.upgradeVisual.mesh;
+                upgradeVisual.meshRenderer.material = upgrade.upgradeVisual.material;
+                return;
+            }
+        }
+    }
+}
--- Assets/Scripts/Upgrades/Scriptable Objects/Upgrade.cs
+++ Assets/Scripts/Upgrades/Scriptable Objects/Upgrade.cs
@@ -4,9 +4,15 @@ namespace Upgrades
 {
     public abstract class Upgrade : ScriptableObject
     {
+        public class UpgradeVisual
+        {
+            public Mesh mesh;
+            public Material material;
+        }
         public string upgradeName;
         public string description;
         public int cost;
+        public UpgradeVisual upgradeVisual;
 
         public abstract string[] GetEffectName();
         public abstract string[] GetUpgradeEffect();
