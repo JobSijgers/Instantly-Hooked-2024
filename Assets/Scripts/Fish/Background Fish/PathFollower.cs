1eaadec6524cb6c96a99cc5300a5084c6971dc58
Job SIjgers <jobsijgers@gmail.com>
2024-05-22 11:21:57 +0200

    backgroundfishspawner

---
+++ Assets/Scripts/Fish/Background Fish/PathFollower.cs
@@ -0,0 +1,32 @@
+﻿using Interfaces;
+using PathCreation;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Fish.Background_Fish
+{
+    public class PathFollower : MonoBehaviour, IBackgroundFish
+    {
+        private Vector3 pathOffset;
+        private PathCreator pathToFollow;
+        private float followSpeed = 5;
+        private float distanceTravelled;
+
+        private void Update()
+        {
+            if (pathToFollow == null) 
+                return;
+            
+            distanceTravelled += followSpeed * Time.deltaTime;
+            transform.position = pathToFollow.path.GetPointAtDistance(distanceTravelled) + pathOffset;
+            transform.rotation = pathToFollow.path.GetRotationAtDistance(distanceTravelled);
+        }
+
+        public void Initialize(PathCreator path, Vector3 offset, float speed)
+        {
+            pathToFollow = path;
+            pathOffset = offset;
+            followSpeed = speed;
+        }
+    }
+}
