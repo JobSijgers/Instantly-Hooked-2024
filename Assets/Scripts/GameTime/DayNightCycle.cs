71f6fd10e8141af77f361c26d8401688a82c1503
Job SIjgers <jobsijgers@gmail.com>
2024-05-23 11:37:38 +0200

    epic lighting go brrr

--- Assets/Scripts/GameTime/DayNightCycle.cs
+++ Assets/Scripts/GameTime/DayNightCycle.cs
@@ -21,6 +21,10 @@ namespace GameTime
     {
         [SerializeField] private List<DayPhase> dayPhases;
         [SerializeField] private Light sun;
+
+        private readonly Vector3 startRotation = new Vector3(50, -180, 0);
+        private readonly Vector3 endRotation = new Vector3(50, 180, 0);
+
         private void Start()
         {
             // Subscribe to TimeUpdate event
@@ -38,13 +42,14 @@ namespace GameTime
             // Convert time to TimeSpan
             TimeSpan timeSpan = TimeSpan.FromSeconds(time);
             float currentTimeInMinutes = (int)timeSpan.TotalMinutes;
+            RotateLight(currentTimeInMinutes / 1440f);
 
             // Iterate over each phase
             foreach (DayPhase phase in dayPhases)
             {
                 // Skip if current time is not within the phase
                 if (timeSpan.Hours < phase.startHour || timeSpan.Hours >= phase.endHour) continue;
-                
+
                 // Calculate start and end time in minutes
                 float startTimeInMinutes = phase.startHour * 60;
                 float endTimeInMinutes = phase.endHour * 60;
@@ -66,10 +71,17 @@ namespace GameTime
                 RenderSettings.skybox.SetTexture("_Texture1", a);
             if (RenderSettings.skybox.GetTexture("_Texture2") != b)
                 RenderSettings.skybox.SetTexture("_Texture2", b);
-
+            
             // Set blend factor
-            sun.color = gradient.Evaluate(t);
             RenderSettings.skybox.SetFloat("_Blend", t);
+
+            //update directional light color and rotation
+            sun.color = gradient.Evaluate(t);
+        }
+        
+        private void RotateLight(float t)
+        {
+            sun.transform.rotation = Quaternion.Euler(Vector3.Lerp(startRotation, endRotation, t));
         }
     }
 }
