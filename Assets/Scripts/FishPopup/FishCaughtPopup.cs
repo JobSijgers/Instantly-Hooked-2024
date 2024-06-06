bb68282e237f1fb54543e905e5388d439eab23a2
Job SIjgers <jobsijgers@gmail.com>
2024-06-06 11:40:42 +0200

    began working on popup

---
+++ Assets/Scripts/FishPopup/FishCaughtPopup.cs
@@ -0,0 +1,24 @@
+﻿using Fish;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace FishPopup
+{
+    [RequireComponent(typeof(ParticleSystem))]
+    public class FishCaughtPopup : MonoBehaviour
+    {
+        [SerializeField] private Image background;
+        [SerializeField] private Image fishImage;
+        [SerializeField] private Image sizeImage;
+        [SerializeField] private ParticleSystem particleSystem;
+
+        public void InitPopup(FishData data, Sprite sprite, Sprite sizeSprite, Color color)
+        {
+            background.sprite = sprite;
+            fishImage.sprite = data.fishVisual;
+            sizeImage.sprite = sizeSprite;
+            ParticleSystem.MainModule particleSystemMain = particleSystem.main;
+            particleSystemMain.startColor = color;
+        }
+    }
+}
