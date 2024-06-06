fa5a7d2b538aa50665dcb79d300b53220c9a5f25
Youri.m <220299@student.glu.nl>
2024-06-06 11:23:20 +0200

    fish stamina werkt

--- Assets/Scripts/Fish/Fish States/FishRoaming.cs
+++ Assets/Scripts/Fish/Fish States/FishRoaming.cs
@@ -2,18 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using Events;
+using Upgrades.Scriptable_Objects;
 public class FishRoaming : MonoBehaviour, IFishState
 {
     private FishBrain Brain;
+
+    [Header("fish intresst")]
     [SerializeField] private float IntresstDistanceToHook;
     [SerializeField] private float BiteWait;
+    public float BiteMultiply;
+
     private Coroutine BiteC;
+
+    [Header("Editor")]
+    [SerializeField] private bool ShowGizmos;
 
     //state change
     private bool BiteState = false;
+
     void Awake()
     {
-        Brain = GetComponent<FishBrain>();  
+        Brain = GetComponent<FishBrain>();
     }
     public void OnStateActivate()
     {
@@ -30,10 +40,12 @@ public class FishRoaming : MonoBehaviour, IFishState
     }
     public void UpdateState()
     {
+        // zet nieuwe End positie als vis positie gelijk staat aan end positie
         if (transform.position == Brain.EndPos)
         {
              SetRandomPosition();
         }
+        // zet nieuwe position als End position zero is
         if (Brain.EndPos == Vector3.zero) { SetRandomPosition(); }
         if (Vector2.Distance(transform.position, Hook.instance.hook.transform.position) < IntresstDistanceToHook 
             && 
@@ -41,12 +53,16 @@ public class FishRoaming : MonoBehaviour, IFishState
         {
             if (BiteC == null) BiteC = StartCoroutine(ChoseToBite());
         }
+
+        // decelaration
         float movespeed = Brain.moveSpeed;
         float dist = Vector2.Distance(transform.position, Brain.EndPos);
         if (dist < 0.6f)
         {
             movespeed *= dist + 0.3f;
         }
+
+        // move fish
         transform.position = Vector3.MoveTowards(transform.position, Brain.EndPos, movespeed * Time.deltaTime);
     }   
     public void SetRandomPosition()
@@ -57,9 +73,11 @@ public class FishRoaming : MonoBehaviour, IFishState
     private IEnumerator ChoseToBite()
     {
         yield return new WaitForSeconds(BiteWait);
-        int RandomValue = Random.Range(1, Brain.fishData.biteRate + 1);
-        int bitevalue = 1;
-        if (RandomValue == bitevalue && Hook.instance.FishOnHook == null)
+        float br = Brain.fishData.biteRate + BiteMultiply;
+        br /= 10f;
+        Debug.Log(br);
+        float randomvalue = Random.value;
+        if (randomvalue < br && Hook.instance.FishOnHook == null)
         {
             Hook.instance.FishOnHook = Brain;
             BiteState = true;
@@ -81,7 +99,7 @@ public class FishRoaming : MonoBehaviour, IFishState
 #if UNITY_EDITOR
     private void OnDrawGizmos()
     {
-        Handles.DrawWireArc(transform.position, Vector3.forward, Vector3.up, 360, IntresstDistanceToHook);
+        if (ShowGizmos) Handles.DrawWireArc(transform.position, Vector3.forward, Vector3.up, 360, IntresstDistanceToHook);
     }
 #endif
 }
