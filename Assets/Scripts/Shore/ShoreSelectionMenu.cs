2a20c67659d3a19cdb4c32fbb7614aeb4392762b
Job Sijgers <jobsijgers@gmail.com>
2024-06-27 20:04:00 +0200

    paar fixes

--- Assets/Scripts/Shore/ShoreSelectionMenu.cs
+++ Assets/Scripts/Shore/ShoreSelectionMenu.cs
@@ -1,10 +1,51 @@
-﻿using Events;
+﻿using System;
+using Events;
+using UnityEngine;
+using UnityEngine.UI;
 using Views;
 
 namespace Shore
 {
     public class ShoreSelectionMenu : ViewComponent
     {
+        [SerializeField] private int notToSeaAfterTime;
+        [SerializeField] private Button seaButton;
+
+        private bool isDead;
+
+        private void Start()
+        {
+            EventManager.TimeUpdate += CheckTime;
+            EventManager.NewDay += EnableSeaButton;
+            EventManager.PlayerDied += PlayerDied;
+        }
+
+        private void OnDestroy()
+        {
+            EventManager.TimeUpdate -= CheckTime;
+            EventManager.NewDay -= EnableSeaButton;
+            EventManager.PlayerDied -= PlayerDied;
+        }
+
+        private void CheckTime(TimeSpan time)
+        {
+            if (isDead)
+                return;
+            seaButton.gameObject.SetActive((time.TotalMinutes < notToSeaAfterTime));
+        }
+
+        private void EnableSeaButton(int arg0)
+        {
+            isDead = false;
+            seaButton.gameObject.SetActive(true);
+        }
+
+        private void PlayerDied()
+        {
+            isDead = true;
+            seaButton.gameObject.SetActive(false);
+        }
+
         public void GoToSea()
         {
             EventManager.OnLeftShore();
